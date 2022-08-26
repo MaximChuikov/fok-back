f4bae5a0904156f6f915f94f9a722a82b31441d4
Maxim <machsim26@gmail.com>
2022-08-26 07:07:34 +0500

    first push

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require('cors')
 const PORT = 8080
 const app = express()
 
-app.use(cors)
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send("<h3>Ролан, привет с нового сервера! :)</h3>")
+    res.status(200).send("<h1>Привет с нового сервера!</h1>")
 })
 
 app.listen(PORT, () => console.log(`Server hosted in ${PORT} port`))
