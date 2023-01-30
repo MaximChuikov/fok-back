692b48b7e985226ec78114364b7b5abfcbf97420
Maxim <machsim26@gmail.com>
2023-01-30 19:22:31 +0500

    delete origin

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    credentials: true,
-    origin: "https://фокбулатова.рф"
+    credentials: true
 }));
 app.use('/api', router);
 app.use(errorMiddleware)
