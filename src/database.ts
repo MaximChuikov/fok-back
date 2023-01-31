96183b99084a1fa10081583d1ae25d774b66df3d
Maxim <machsim26@gmail.com>
2023-01-31 11:51:24 +0500

    delete file db

--- src/database.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const Pool = require('pg').Pool
-export const pool = new Pool({
-    user: process.env.user,
-    password: process.env.password,
-    host: process.env.host,
-    port: process.env.port,
-    database: process.env.database
-})
