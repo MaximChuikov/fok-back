9719608307d2e20faa3dbc06def4487380744cb2
Maxim <machsim26@gmail.com>
2023-02-21 18:53:38 +0500

    ready project

--- src/index.ts
+++ src/index.ts
@@ -92,8 +92,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    //origin: ['https://фокбулатова.рф', 'http://localhost:3000']
-    origin: 'https://фокбулатова.рф'
+    origin: ['https://фокбулатова.рф']
 }));
 app.use('/api', router);
 app.use(errorMiddleware)
